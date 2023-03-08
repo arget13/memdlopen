3267ebe087e75b923e71bace56bc8c7e38cd52d5
Yago Gutiérrez <yagogl@protonmail.com>
2023-03-08 02:34:44 +0100

    First upload

---
+++ memdlopen.c
@@ -0,0 +1,337 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <link.h>
+
+#include <elf.h>
+
+#include <sys/stat.h>
+#include <libgen.h>
+
+#define FAKE_LIB "asd"
+#define MAGIC_FD 0x1337
+#if defined(__x86_64__)
+    #define SYSCALL_INST 0x050f
+    typedef uint16_t inst_t;
+    #define ILL_INST 0x0b0f
+#elif defined(__aarch64__)
+    #define SYSCALL_INST 0xd4000001
+    typedef uint32_t inst_t;
+    #define ILL_INST 0x00000000
+#endif
+
+#define ISPIE 1
+#define ISLIB 2
+
+uint8_t* lib_addr;
+size_t lib_fsize;
+uint64_t lib_base;
+
+void* search_syscall(void* start, void* stop)
+{
+    for(uint8_t* ptr = start; (void*) ptr < (stop - sizeof(inst_t) - 1); ++ptr)
+        if(*(inst_t*) ptr == SYSCALL_INST)
+            *(inst_t*) ptr = ILL_INST;
+}
+
+#if defined(__x86_64__)
+struct sigcontext_t
+{
+    uint64_t _1[5];
+    uint64_t arg5; // r8
+    uint64_t arg6; // r9
+    uint64_t arg4; // r10
+    uint64_t _2[5];
+    uint64_t arg1; // rdi
+    uint64_t arg2; // rsi
+    uint64_t _3[2];
+    uint64_t arg3; // rdx
+    uint64_t nr;   // rax
+    uint64_t _4[2];
+    uint64_t pc;   // rip
+};
+#elif defined(__aarch64__)
+struct sigcontext_t
+{
+    uint64_t arg1; // x0
+    uint64_t arg2; // x1
+    uint64_t arg3; // x2
+    uint64_t arg4; // x3
+    uint64_t arg5; // x4
+    uint64_t arg6; // x5
+    uint64_t _1[2];
+    uint64_t nr;   // x8
+    uint64_t _2[23];
+    uint64_t pc;
+};
+#endif
+
+void sigill_handler(int s)
+{
+    struct sigcontext_t* sigctx;
+    uint64_t  nr, arg1, arg2, arg3, arg4, arg5, arg6, * ret;
+    char* fmt = NULL;
+    static uint64_t fakefp = 0;
+
+    #if defined(__x86_64__)
+    asm volatile("lea 0x10(%%rbp), %0;"
+                 : "=r" (sigctx));
+    ret = &sigctx->nr;
+    #elif defined(__aarch64__)
+    asm("ldr x28, [sp];"
+        "sub  %0, x28, #0x1000;"
+        "sub  %0,  %0, #0x0118;"
+        : "=r" (sigctx) : : "x28");
+    ret = &sigctx->arg1;
+    #endif
+    nr  = sigctx->nr;
+    arg1  = sigctx->arg1;
+    arg2  = sigctx->arg2;
+    arg3  = sigctx->arg3;
+    arg4  = sigctx->arg4;
+    arg5  = sigctx->arg5;
+    arg6  = sigctx->arg6;
+    sigctx->pc += sizeof(inst_t); // Can't leave the PC pointing to the ill instruction
+
+    switch(nr)
+    {
+        case SYS_pread64:
+            fmt = "pread(0x%lx, 0x%lx, 0x%lx, 0x%lx)";
+            if(arg1 != MAGIC_FD)
+                goto legit_syscall;
+            (uint64_t) memcpy((void*) arg2, lib_addr + arg4, arg3);
+            *ret = arg3;
+            break;
+        case SYS_read:
+            fmt = "read(0x%lx, 0x%lx, 0x%lx)";
+            if(arg1 != MAGIC_FD)
+                goto legit_syscall;
+            (uint64_t) memcpy((void*) arg2, lib_addr + fakefp, arg3);
+            fakefp += arg3;
+            *ret = arg3;
+            break;
+        case SYS_mmap:
+            fmt = "mmap(0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)";
+            if(arg5 != MAGIC_FD)
+                goto legit_syscall;
+            uint64_t flags = MAP_PRIVATE | MAP_ANONYMOUS | (arg4 & MAP_FIXED);
+            *ret =
+                (uint64_t) mmap((void*) arg1, arg2,
+                                PROT_READ | PROT_WRITE,
+                                flags, -1, 0);
+            memcpy((void*) *ret, lib_addr + arg6,
+                   ((arg6 + arg2) > lib_fsize) ? lib_fsize - arg6 : arg2);
+            mprotect((void*) *ret, arg2, arg3);
+            if(arg6 == 0) lib_base = *ret;
+            break;
+        case SYS_openat:
+            fmt = "openat(0x%lx, %s, 0x%lx)";
+            if(strcmp(basename((char*) arg2), FAKE_LIB))
+                goto legit_syscall;
+            *ret = MAGIC_FD;
+            break;
+        case SYS_fstat:
+            fmt = "fstat(0x%lx, 0x%lx)";
+        case SYS_newfstatat:
+            if(nr == SYS_newfstatat) fmt = "fstatat(0x%lx, %s, 0x%lx, 0x%lx)";
+            if(arg1 != MAGIC_FD)
+                goto legit_syscall;
+            struct timespec ts = { 0 };
+            struct stat* st = (struct stat*) ((nr == SYS_newfstatat) ? arg3 : arg2);
+            st->st_dev = 0xdeadbeef;
+            st->st_ino = 0xcafebabe;
+            st->st_mode = S_IFREG | 0755;
+            st->st_nlink = 1;
+            st->st_uid = 0;
+            st->st_gid = 0;
+            st->st_rdev = 0;
+            st->st_size = lib_fsize;
+            st->st_blksize = 4096;
+            st->st_blocks = lib_fsize / 512 + !!(lib_fsize % 512);
+            st->st_atim = ts;
+            st->st_mtim = ts;
+            st->st_ctim = ts;
+            *ret = 0;
+            break;
+        case SYS_close:
+            fmt = "close(0x%lx)";
+            if(arg1 != MAGIC_FD)
+                goto legit_syscall;
+            *ret = 0;
+            break;
+        case SYS_mprotect:
+            fmt = "mprotect(0x%lx, 0x%lx, 0x%lx)";
+        case SYS_munmap:
+            fmt = "munmap(0x%lx, 0x%lx)";
+        default:
+            goto legit_syscall;
+    }
+    if(getenv("DEBUG"))
+    {
+        printf("Simulated : ");
+        printf(fmt, arg1, arg2, arg3, arg4, arg5, arg6, nr);
+        printf(" = 0x%lx\n", *ret);
+    }
+    return;
+
+    legit_syscall:
+    *ret = syscall(nr, arg1, arg2, arg3, arg4, arg5, arg6);
+    if(fmt == NULL)
+        fmt = "syscall(0x%7$lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx, 0x%lx)";
+
+    if(getenv("DEBUG"))
+    {
+        printf("Detected  : ");
+        printf(fmt, arg1, arg2, arg3, arg4, arg5, arg6, nr);
+        printf(" = 0x%lx\n", *ret);
+    }
+    return;
+}
+
+uint64_t search_section(void* elf, char* section)
+{
+    Elf64_Ehdr* ehdr = elf;
+    Elf64_Shdr* shdr = elf + ehdr->e_shoff;
+    uint16_t shnum = ehdr->e_shnum;
+    uint16_t shstrndx = ehdr->e_shstrndx;
+    char* shstrtab = elf + shdr[shstrndx].sh_offset;
+
+    for(int i = 0; i < shnum; ++i)
+        if(!strcmp(&shstrtab[shdr[i].sh_name], section))
+            return shdr[i].sh_offset;
+    return 0;
+}
+
+int patch_elf()
+{
+    int ret = ISPIE | ISLIB;
+    Elf64_Ehdr* ehdr = (Elf64_Ehdr*) lib_addr;
+    Elf64_Dyn* dyn = (void*) lib_addr + search_section(lib_addr, ".dynamic");
+    uint16_t shnum = ehdr->e_shnum;
+
+    if(ehdr->e_type == ET_EXEC)
+    {
+        ehdr->e_type = ET_DYN;
+        return 0; // Not PIE and therefore not a library
+    }
+
+    for(int i = 0; i < shnum; ++i)
+        if((dyn[i].d_tag == DT_FLAGS_1) && (dyn[i].d_tag & DF_1_PIE))
+        {
+            dyn[i].d_tag &= ~DF_1_PIE;
+            return ISPIE; // PIE but not a library
+        }
+    return ISPIE | ISLIB; // A library
+}
+
+#define READ_SIZE 0x10000
+void read_elf()
+{
+    size_t r = 0;
+
+    lib_addr = malloc(READ_SIZE);
+    while((r = read(0, &lib_addr[lib_fsize], READ_SIZE)) == READ_SIZE)
+    {
+        lib_fsize += r;
+        lib_addr = realloc(lib_addr, lib_fsize + READ_SIZE);
+    }
+    lib_fsize += r;
+}
+
+void* ld_addr(size_t* len)
+{
+    FILE* f = fopen("/proc/self/maps", "rb");
+    char buf[1024];
+    void* p1, * p2;
+    while(fgets(buf, sizeof buf, f))
+    {
+        if(strncmp(basename(strchr(buf, '/')), "ld", 2)) continue;
+        if(strncmp(strchr(buf, ' ') + 1, "r-x", 3)) continue;
+        sscanf(buf, "%lx-%lx", &p1, &p2);
+        *len = p2 - p1;
+        fclose(f);
+        return p1;
+    }
+    fclose(f);
+    return NULL;
+}
+
+void fini()
+{
+    _exit(0);
+}
+
+int main(int argc, char** argv)
+{
+    void** sp;
+    char* stack;
+    struct link_map* map;
+    uint64_t entry;
+    int type;
+
+    size_t len;
+    void* ld, *h;
+    read_elf();
+    type = patch_elf();
+
+    signal(SIGILL, sigill_handler);
+    ld = ld_addr(&len);
+    mprotect(ld, len, PROT_READ | PROT_WRITE);
+    search_syscall(ld, ld + len);
+    mprotect(ld, len, PROT_READ | PROT_EXEC);
+
+    h = dlopen(FAKE_LIB, RTLD_NOW | RTLD_GLOBAL);
+    if(h == NULL)
+    {
+        printf("Error in dlopen(): %s.\n", dlerror());
+        return 1;
+    }
+    free(lib_addr);
+
+    if(type & ISLIB)
+    {
+        ((void (*)(char*))dlsym(h, argv[1]))(argv[2]);
+        return 0;
+    }
+
+    if(type & ISPIE)
+        entry = ((Elf64_Ehdr*) lib_base)->e_entry + lib_base;
+    else
+        entry = ((Elf64_Ehdr*) lib_base)->e_entry;
+
+    stack = mmap(NULL, 0x21000, PROT_READ | PROT_WRITE,
+                 MAP_ANONYMOUS | MAP_PRIVATE | MAP_STACK, -1, 0);
+    sp = (void**) &stack[0x21000];
+    *--sp = NULL; // End of stack
+    argc--; argv++;
+
+    if(argc % 2)
+        *--sp = NULL; // Keep stack aligned
+    *--sp = NULL;     // End of envp
+    *--sp = NULL;     // End of argv
+    sp -= argc; memcpy(sp, argv, argc * 8);
+    *(size_t*) --sp = argc;
+
+    #if defined(__x86_64__)
+    asm volatile("mov %0, %%rsp;"
+                 "jmp *%1;"
+                 : : "r"(sp), "r"(entry), "d"(fini));
+    #elif defined(__aarch64__)
+    asm volatile("mov x1, sp;"
+                 "sub sp, sp, x1;"
+                 "add sp, sp, %0;"
+                 "mov x0, %2;"
+                 "br  %1;"
+                 : : "r"(sp), "r"(entry), "r"(fini) : "x0", "x1");
+    #endif
+
+    __builtin_unreachable();
+    return 0;
+}
---
+++ memdlopen.s
@@ -0,0 +1,271 @@
+.arch armv8-a
+.global _start
+
+_start:
+    // Read ELF in memory
+        // b       .
+        mov     x8, #0xde // mmap
+        mov     x2, #3    // RW
+        mov     x3, #0x22 // MAP_ANON | MAP_PRIVATE
+        movn    x4, #0    // -1
+        mov     x5, #0
+        adr     x19, data
+        ldr     x1, [x19] // lib_fsize
+        mov     x0, #0
+        svc     #0
+        ldr     x25, [x19, #0x20] // stack_top
+        str     x0,  [x25, #-8]   // lib_addr
+        stp     xzr, xzr, [x25, #-0x18]
+        mov     x8, #0x3f // read
+        mov     x2, x1
+        mov     x1, x0
+        mov     x0, #9 // fd
+        svc     #0
+
+    // Install signal handler for SIGILL
+        mov     x9, #0xa
+    memset:
+        stp     xzr, xzr, [sp, #-0x10]!
+        sub     x9, x9, #1
+        cmp     x9, #0
+        bne     memset
+        adr     x9, sigill_handler
+        str     x9, [sp]  // Now we have a struct sigaction in stack
+        mov     x0, #4    // SIGILL
+        mov     x1, sp
+        mov     x2, #0
+        mov     x3, #8
+        mov     x8, #0x86 // sigaction
+        svc     #0
+
+    // Change svc #0 instructions in loader with undefined ones
+        mov     x8, #0xe2         // mprotect
+        ldr     x10, [x19, #0x08] // ld_end_addr
+        ldr     x9,  [x19, #0x10] // ld_start_addr
+        sub     x1, x10, x9
+        mov     x2, #3 // RW
+        mov     x0, x9
+        svc     #0
+        bl      search_syscall
+        sub     x1, x10, x9
+        mov     x2, #5 // RX
+        mov     x0, x9
+        svc     #0
+
+    // Call dlopen(FAKE_LIB, RTLD_NOW | RTLD_GLOBAL)
+        ldr     x2, [x19, #0x18] // libdl base address
+        add     x2, x2, #0x1000
+        add     x2, x2, #0x0100  // dlopen offset = 0x1100
+        adr     x0, FAKE_LIB
+        mov     x1, #0x102 // RLTD_NOW | RTLD_GLOBAL
+        blr     x2
+
+        movn    x8, #0x1000 // non-existent syscall
+        udf     #1234 // breakpoint (kinda)
+        adr     x19, data
+        ldr     x19, [x19, #0x20] // stack_top
+        ldr     x13, [x19, #-0x18] // lib_base
+        add     x13, x13, #0x0780 // entrypoint
+        add     x13, x13, #0x5000
+        blr     x13
+        .word   0xfabada // won't be hit
+
+
+
+search_syscall:
+        mov     x0, x9
+        movz    x1, #0xd400, lsl #0x10
+        orr     x1, x1, #1
+        b       check
+    loop:
+        ldr     w2, [x0], #4
+        cmp     w2, w1
+        bne     check
+        str     wzr, [x0, #-4]
+    check:
+        cmp     x0, x10
+        blo     loop
+        ret
+
+sigill_handler:
+        // Can't leave PC pointing to ILL instruction
+        ldr     x9, [sp, #0x238]
+        add     x9, x9, #4
+        str     x9, [sp, #0x238]
+
+        // All arguments intended for the syscall
+        ldp     x0, x1, [sp, #0x138]
+        ldp     x2, x3, [sp, #0x148]
+        ldp     x4, x5, [sp, #0x158]
+        ldr     x8, [sp, #0x178]
+
+        adr     x19, data
+        ldr     x25, [x19, #0x20] // stack_top
+        mov     x11, #0x1337
+
+        cmp     x8, #0x3f
+        beq     sim_read
+        cmp     x8, #0xde
+        beq     sim_mmap
+        cmp     x8, #0x38
+        beq     sim_openat
+        cmp     x8, #0x50
+        beq     sim_fstat
+        cmp     x8, #0x4f
+        beq     sim_newfstatat
+        cmp     x8, #0x39
+        beq     sim_close
+        b       legit_syscall
+
+sim_read:
+        cmp     x0, x11
+        bne     legit_syscall
+        mov     x11, #0
+        ldr     x20, [x25, #-8]    // lib_addr
+        ldr     x21, [x25, #-0x10] // fakefp
+        add     x12, x20, x21 // x12 = lib_addr + fakefp
+    sim_read_1:
+        cmp     x11, x2
+        beq     sim_read_2
+        ldrb    w10, [x12, x11]
+        strb    w10, [ x1, x11]
+        add     x11, x11, #1
+        b       sim_read_1
+    sim_read_2:
+        str     x2, [sp, #0x138]
+        add     x21, x21, x2
+        str     x21, [x25, #-0x10]
+        ret
+
+sim_mmap:
+        cmp     x4, x11
+        bne     legit_syscall
+        mov     x13, x2
+        mov     x14, x5
+        mov     x2, #3        // RW
+        and     x3, x3, #0x10 // MAP_FIXED
+        mov     x11, 0x22
+        orr     x3, x3, x11   // MAP_ANON | MAP_PRIVATE
+        movn    x4, #0        // -1
+        mov     x5, #0
+        svc     #0
+        cmp     x14, #0
+        bne     sim_mmap_1
+        str     x0, [x25, #-0x18] // lib_base
+    sim_mmap_1:
+        str     x0, [sp, #0x138]
+        add     x11, x1, x14 // x11 = size + offset
+        ldr     x19, [x19]   // lib_fsize
+        cmp     x11, x19
+        bls     sim_mmap_2
+        sub     x19, x19, x14
+    sim_mmap_2: // memcpy
+        mov     x11, #0
+        ldr     x20, [x25, #-8] // lib_addr
+        add     x12, x20, x14
+    sim_mmap_3:
+        cmp     x11, x19
+        beq     sim_mmap_4
+        ldrb    w10, [x12, x11]
+        strb    w10, [ x0, x11]
+        add     x11, x11, #1
+        b       sim_mmap_3
+    sim_mmap_4:
+        mov     x8, #0xe2 // mprotect
+        mov     x2, x13
+        svc     #0
+        ret
+
+sim_openat:
+        mov     x9, x1
+    sim_openat_1:
+        ldrb    w12, [x9], #1
+        cmp     x12, #0
+        bne     sim_openat_1
+        sub     x9, x9, #1 // x9 pointing to null terminator
+    sim_openat_2:
+        ldrb    w12, [x9, #-1]!
+        cmp     x12, #0x2f // '/'
+        bne     sim_openat_2
+        add     x9, x9, #1 // x9 now pointing (in theory) to basename
+        adr     x10, FAKE_LIB
+    sim_openat_3:
+        ldrb    w12, [x10], #1
+        ldrb    w11, [ x9], #1
+        cmp     x11, x12
+        bne     legit_syscall
+        cmp     x11, #0
+        beq     sim_openat_4
+        b       sim_openat_3
+    sim_openat_4:
+        mov     x21, #0
+        str     x21, [x25, #-0x10]
+        mov     x0, #0x1337
+        str     x0, [sp, 0x138]
+        ret
+
+sim_fstat:
+        mov     x2, x1
+sim_newfstatat:
+        cmp     x0, x11
+        bne     legit_syscall
+        // st_dev
+        movz    x9, #0xdead, lsl #0x10
+        movk    x9, #0xbeef
+        str     x9, [x2, #0x00]
+        // st_ino
+        movz    x9, #0xcafe, lsl #0x10
+        movk    x9, #0xbabe
+        str     x9, [x2, #0x08]
+        // st_nlink
+        mov     x9, #1
+        str     x9, [x2, #0x10]
+        // st_mode
+        mov     x9, #0x81ed
+        str     w9, [x2, #0x18]
+        // st_uid and st_gid
+        str     xzr, [x2, #0x1c]
+        // st_rdev
+        str     xzr, [x2, #0x28]
+        // st_size
+        ldr     x19, [x19] // lib_fsize
+        str     x19, [x2, #0x30]
+        // st_blksize
+        mov     x9, #0x1000
+        str     x9, [x2, #0x38]
+        // st_blocks
+        mov     x10, #0x200
+        udiv    x9, x19, x10
+        msub    x10, x9, x10, x19
+        cmp     x10, #0
+        beq     sim_newfstat_1
+        add     x9, x9, #1
+    sim_newfstat_1:
+        str     x9, [x2, #0x40]
+        // st_atim, st_mtim and st_ctim
+        stp     xzr, xzr, [x2, #0x48]
+        stp     xzr, xzr, [x2, #0x58]
+        stp     xzr, xzr, [x2, #0x68]
+        str     xzr, [sp, 0x138]
+        ret
+
+sim_close:
+        cmp     x0, x11
+        bne     legit_syscall
+        str     xzr, [sp, #0x138]
+        ret
+
+legit_syscall:
+        svc     #0
+        str     x0, [sp, #0x138]
+        ret
+
+
+FAKE_LIB: .asciz "asd"
+.align
+data    :
+// lib_fsize     : .dword 0
+// ld_end_addr   : .dword 0
+// ld_start_addr : .dword 0
+// libdl_addr    : .dword 0
+// stack_top     : .dword 0
